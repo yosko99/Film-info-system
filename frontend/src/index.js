3823522b5e561d9d5bc4481488d7a18ad3e6a2a4
yosko99 <thedriftmaniaka1997@gmail.com>
2022-03-23 17:08:07 +0200

    Add axios load bar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,11 +1,15 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { ReactQueryDevtools } from 'react-query/devtools';
+import { loadProgressBar } from 'axios-progress-bar';
 import reportWebVitals from './reportWebVitals';
+import 'axios-progress-bar/dist/nprogress.css';
 import ReactDOM from 'react-dom';
 import './bootstrap.min.css';
 import React from 'react';
 import App from './App';
 import './index.css';
+
+loadProgressBar();
 
 const queryClient = new QueryClient({
   defaultOptions: {
